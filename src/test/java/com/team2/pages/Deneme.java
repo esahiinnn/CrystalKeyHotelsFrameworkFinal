4f50bd3196d838427219a756709f9a9185915203
esahiinnn <esahin3@icloud.com>
2020-12-25 20:06:34 +0300

    First

---
+++ src/test/java/com/team2/pages/Deneme.java
@@ -0,0 +1,4 @@
+package com.team2.pages;
+
+public class Deneme {
+}
---
+++ src/test/java/com/team2/utilities/Driver.java
@@ -0,0 +1,45 @@
+package com.team2.utilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.OperaDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import java.util.concurrent.TimeUnit;
+public class Driver {
+    private Driver(){
+    }
+    static WebDriver driver;
+    static public WebDriver getDriver(){
+        if(driver==null){
+            switch (ConfigReader.getProperty("browser")){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.getInstance(SafariDriver.class);
+                    driver=new SafariDriver();
+                    break;
+                case "opera":
+                    OperaDriverManager.operadriver().setup();
+                    driver=new OperaDriver();
+                    break;
+            }
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+    public static void closeDriver(){
+        if (driver!=null){
+            driver.close();
+            driver=null;
+        }
+    }
+}
